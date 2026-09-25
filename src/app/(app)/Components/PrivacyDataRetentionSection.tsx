607bb942221e27139824c87b53f56bea4e92af1c
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-25 15:53:43 +0500

    Add PrivacyDataRetentionSection component

---
+++ src/app/(app)/Components/PrivacyDataRetentionSection.tsx
@@ -0,0 +1,13 @@
+export default function PrivacyDataRetentionSection() {
+  return (
+    <div>
+      <h2 className="font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl">
+        Data Retention Policy
+      </h2>
+
+      <p className="mt-5 font-manrope text-base font-normal leading-8 text-neutral-500 md:text-lg">
+        We retain PHI only as long as required by law or contract. Once data is no longer needed, it is securely deleted or destroyed in accordance with HIPAA’s data disposal requirements.
+      </p>
+    </div>
+  );
+}
