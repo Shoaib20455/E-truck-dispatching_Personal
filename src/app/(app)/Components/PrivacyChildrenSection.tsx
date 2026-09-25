070c7373508612becaf71b00fa2bfecd3bb6b573
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-25 15:53:46 +0500

    Add PrivacyChildrenSection component

---
+++ src/app/(app)/Components/PrivacyChildrenSection.tsx
@@ -0,0 +1,13 @@
+export default function PrivacyChildrenSection() {
+  return (
+    <div>
+      <h2 className="font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl">
+        Children’s Privacy
+      </h2>
+
+      <p className="mt-5 font-manrope text-base font-normal leading-8 text-neutral-500 md:text-lg">
+        Our services do not target children under 13, and we do not knowingly collect personal data from minors without proper legal or parental consent.
+      </p>
+    </div>
+  );
+}
