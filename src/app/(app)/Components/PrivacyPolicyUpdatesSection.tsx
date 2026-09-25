73499cf77a60e8294addba506fe7890a5f5ec40c
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-25 15:53:49 +0500

    Add PrivacyPolicyUpdatesSection component

---
+++ src/app/(app)/Components/PrivacyPolicyUpdatesSection.tsx
@@ -0,0 +1,16 @@
+export default function PrivacyPolicyUpdatesSection() {
+  return (
+    <div>
+      <h2 className="font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl">
+        Policy Updates
+      </h2>
+
+      <div className="mt-5 font-manrope text-base font-normal leading-8 text-neutral-500 md:text-lg">
+        <p>
+          We may update this Privacy Policy to reflect changes in regulations, technology, or business practices. We will post all updates on our website with a revised effective date.
+        </p>
+        <p>Please review this page periodically to stay informed about how we protect your information.</p>
+      </div>
+    </div>
+  );
+}
