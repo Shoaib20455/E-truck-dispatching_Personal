1588bdab237f31ffeaae64530519e8a63a6403c3
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-25 14:47:59 +0500

    Add SectionHeading component

---
+++ src/app/(app)/Components/AboutUs/SectionHeading.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from "react";
+
+export default function SectionHeading({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle?: ReactNode;
+}) {
+  return (
+    <div className="text-center">
+      <h2 className="font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl">
+        {title}
+      </h2>
+
+      {subtitle && (
+        <div className="mx-auto mt-5 max-w-3xl font-manrope text-base font-normal leading-8 text-neutral-500 md:text-lg">
+          {subtitle}
+        </div>
+      )}
+    </div>
+  );
+}
