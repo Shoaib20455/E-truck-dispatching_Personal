065b19d3d2dbaeb2ecb66e3a30c7ba8232a21991
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-25 15:43:01 +0500

    Build responsive contact us page

--- src/app/(app)/contact-us/page.tsx
+++ src/app/(app)/contact-us/page.tsx
@@ -1,9 +1,24 @@
+import ContactDetailsSection from "../Components/ContactDetailsSection";
+import ServiceHero from "../Components/ServiceHero";
 
-export default function contactUsPage() {
+export default function ContactUsPage() {
   return (
     <main>
-        
+      <ServiceHero
+        variant="compact"
+        heading="Let's Connect Now!"
+        description="If you are interested in our services, want to know more or have got any question's, We would be glad to answer your query. Get in touch now to find out how we can skyrocket your practice growth."
+        backgroundImage="/contact us/2_rectangle_1893.png"
+        highlights={[]}
+        formHeading="Ready to Improve Your Medical Billing Revenue?"
+        namePlaceholder="Name"
+        phonePlaceholder="Phone Number"
+        emailPlaceholder="Email"
+        organizationPlaceholder="Organization"
+        buttonText="Submit"
+      />
 
-  </main>
+      <ContactDetailsSection />
+    </main>
   );
 }
