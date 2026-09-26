ad35befd2c9f415b99b6a7e99d98380d6e93a832
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-26 10:48:13 +0500

    Add contextual reveal motion to WhyChooseUsSection

--- src/app/(app)/Components/WhyChooseUsSection.tsx
+++ src/app/(app)/Components/WhyChooseUsSection.tsx
@@ -1,3 +1,5 @@
+import { AnimatedSection } from "./animation/MotionElements";
+
 import SectionHeading from "./SectionHeading";
 
 export type WhyChooseUsItem = {
@@ -15,7 +17,7 @@ export default function WhyChooseUsSection({
   items,
 }: WhyChooseUsSectionProps) {
   return (
-    <section className="w-full bg-cyan-50 py-12 lg:py-16">
+    <AnimatedSection preset="fade-up" className="w-full bg-cyan-50 py-12 lg:py-16">
       <div className="mx-auto w-full max-w-[1520px] px-6 lg:px-8 2xl:px-0">
         <SectionHeading title={heading} />
 
@@ -40,6 +42,6 @@ export default function WhyChooseUsSection({
           </div>
         </div>
       </div>
-    </section>
+    </AnimatedSection>
   );
 }
