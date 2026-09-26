235b7db42c0b4f7ab12d28ab402078de4dec8999
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-26 13:52:01 +0500

    Upgrade state revenue banner motion

--- src/app/(app)/Components/StateRevenueBanner.tsx
+++ src/app/(app)/Components/StateRevenueBanner.tsx
@@ -33,7 +33,7 @@ export default function StateRevenueBanner({
           style={{ backgroundImage: `url("${backgroundImage}")` }}
         >
           <div className="grid grid-cols-1 items-end gap-8 px-8 pt-10 md:px-12 lg:grid-cols-[1fr_auto] lg:px-16 lg:pt-12">
-            <AnimatedDiv preset="slide-left" className="pb-10 text-white lg:max-w-2xl lg:pb-12">
+            <AnimatedDiv preset="panel-left" className="pb-10 text-white lg:max-w-2xl lg:pb-12">
               <AnimatedHeading
                 text={heading}
                 className="mb-5 font-inter text-3xl font-semibold leading-tight md:text-4xl lg:text-5xl"
@@ -60,9 +60,9 @@ export default function StateRevenueBanner({
               </StaggerGroup>
             </AnimatedDiv>
 
-            <ImageReveal className="flex justify-center lg:justify-end">
+            <AnimatedDiv preset="map-pop" className="flex justify-center lg:justify-end">
               <Image src={doctorImage} alt={doctorAlt} width={520} height={700} sizes="(min-width: 1024px) 390px, 330px" className="h-auto w-full max-w-[330px] object-contain lg:-mt-20 lg:max-w-[390px]" />
-            </ImageReveal>
+            </AnimatedDiv>
           </div>
         </div>
       </div>
