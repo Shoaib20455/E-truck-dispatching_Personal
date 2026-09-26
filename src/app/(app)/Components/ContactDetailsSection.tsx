531954f8b9ec93beb530e0bb9cdfdae414440a32
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-26 13:10:02 +0500

    Add directional motion to contact details

--- src/app/(app)/Components/ContactDetailsSection.tsx
+++ src/app/(app)/Components/ContactDetailsSection.tsx
@@ -1,4 +1,4 @@
-import { AnimatedSection } from "./animation/MotionElements";
+import { AnimatedDiv, AnimatedHeading, AnimatedSection } from "./animation/MotionElements";
 
 import ContactConsultationForm, { type ContactFormCopy } from "./ContactConsultationForm";
 import ContactInformationPanel, { type ContactCard, type SocialLinkItem } from "./ContactInformationPanel";
@@ -32,13 +32,14 @@ export default function ContactDetailsSection({
   return (
     <AnimatedSection preset="fade-up" className="w-full bg-cyan-50 py-16 lg:py-20">
       <div className="mx-auto w-full max-w-[1520px] px-6 lg:px-8 2xl:px-0">
-        <h2 className="mx-auto max-w-[660px] whitespace-pre-line text-center font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl md:leading-[1.15]">
-          {heading}
-        </h2>
+        <AnimatedHeading
+          text={heading}
+          className="mx-auto max-w-[660px] whitespace-pre-line text-center font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl md:leading-[1.15]"
+        />
 
         <div className="mt-14 grid grid-cols-1 items-start gap-12 lg:grid-cols-[minmax(0,1fr)_minmax(0,1.05fr)] lg:gap-20">
-          <ContactInformationPanel {...information} />
-          <ContactConsultationForm {...form} />
+          <AnimatedDiv preset="slide-left"><ContactInformationPanel {...information} /></AnimatedDiv>
+          <AnimatedDiv preset="slide-right"><ContactConsultationForm {...form} /></AnimatedDiv>
         </div>
       </div>
     </AnimatedSection>
