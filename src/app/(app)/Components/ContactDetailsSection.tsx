bd73c269a909bb959997becbe80c87f8f12364c3
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-26 10:45:02 +0500

    Add contextual reveal motion to ContactDetailsSection

--- src/app/(app)/Components/ContactDetailsSection.tsx
+++ src/app/(app)/Components/ContactDetailsSection.tsx
@@ -1,3 +1,5 @@
+import { AnimatedSection } from "./animation/MotionElements";
+
 import ContactConsultationForm, { type ContactFormCopy } from "./ContactConsultationForm";
 import ContactInformationPanel, { type ContactCard, type SocialLinkItem } from "./ContactInformationPanel";
 
@@ -28,7 +30,7 @@ export default function ContactDetailsSection({
   form,
 }: ContactDetailsSectionProps) {
   return (
-    <section className="w-full bg-cyan-50 py-16 lg:py-20">
+    <AnimatedSection preset="fade-up" className="w-full bg-cyan-50 py-16 lg:py-20">
       <div className="mx-auto w-full max-w-[1520px] px-6 lg:px-8 2xl:px-0">
         <h2 className="mx-auto max-w-[660px] whitespace-pre-line text-center font-inter text-4xl font-semibold leading-tight text-heading md:text-5xl md:leading-[1.15]">
           {heading}
@@ -39,6 +41,6 @@ export default function ContactDetailsSection({
           <ContactConsultationForm {...form} />
         </div>
       </div>
-    </section>
+    </AnimatedSection>
   );
 }
