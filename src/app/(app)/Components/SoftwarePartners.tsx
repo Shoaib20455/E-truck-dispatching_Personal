d6ae78be82d0785d8ec11103599fb63f9d99648c
Muhammad Shoaib <139552464+Shoaib20455@users.noreply.github.com>
2026-09-26 10:52:09 +0500

    Animate software partner logos

--- src/app/(app)/Components/SoftwarePartners.tsx
+++ src/app/(app)/Components/SoftwarePartners.tsx
@@ -1,4 +1,4 @@
-import { AnimatedSection } from "./animation/MotionElements";
+import { AnimatedSection, StaggerGroup, StaggerItem } from "./animation/MotionElements";
 
 import Image from "next/image";
 
@@ -49,14 +49,11 @@ const softwarePartners = [
 
 export default function SoftwarePartners() {
   return (
-    <AnimatedSection preset="fade-up" className="w-full bg-cyan-50">
+    <AnimatedSection preset="fade" className="w-full bg-cyan-50">
       <div className="mx-auto max-w-[1520px] px-6 py-10 lg:px-0">
-        <div className="flex flex-wrap items-center justify-center gap-3">
+        <StaggerGroup stagger={0.05} className="flex flex-wrap items-center justify-center gap-3">
           {softwarePartners.map((partner) => (
-            <div
-              key={partner.name}
-              className="flex items-center justify-center rounded-[10px] bg-teal-500/10 px-7 py-4"
-            >
+            <StaggerItem key={partner.name} preset="logo" hover="soft" className="flex items-center justify-center rounded-[10px] bg-teal-500/10 px-7 py-4">
               <Image
                 src={partner.src}
                 alt={partner.name}
@@ -64,9 +61,9 @@ export default function SoftwarePartners() {
                 height={partner.height}
                 className="block h-[42px] w-auto object-contain"
               />
-            </div>
+            </StaggerItem>
           ))}
-        </div>
+        </StaggerGroup>
       </div>
     </AnimatedSection>
   );
